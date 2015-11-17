956756bd86c43375df89ede4e251e9e78c6dff0c
mrhubbs <semberwj@gmail.com>
2015-11-17 12:03:58 -0500

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+11-13-15
+"""
+
+
+from dna import DNA
+from dna_chain import DNANode, DNACrawlerException
+
+
+__globals__ = ('DNA',
+               'DNANode',
+               'DNACrawlerException')
