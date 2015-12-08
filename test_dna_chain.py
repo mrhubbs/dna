3f7b258e82309b069be59c2300cc76a2f0cba30c
mrhubbs <semberwj@gmail.com>
2015-12-08 13:18:26 -0500

    Update for API name changes.

--- test_dna_chain.py
+++ test_dna_chain.py
@@ -57,29 +57,29 @@ class tests(test_utils):
     # test inserting (before, after, child)
 
     def test_1_insert_before(self):
-        self.crawler.insert_before(self.n2)
+        self.crawler.add_before(self.n2)
         self.check_seq(self.n2, self.n1)
 
         self.assertIsNone(self.n2.dna_node_prev_sib)
         self.assertIsNone(self.n1.dna_node_next_sib)
 
     def test_2_insert_after(self):
-        self.crawler.insert_after(self.n2)
+        self.crawler.add_after(self.n2)
         self.check_seq(self.n1, self.n2)
 
         self.assertIsNone(self.n1.dna_node_prev_sib)
         self.assertIsNone(self.n2.dna_node_next_sib)
 
     def test_3_insert_child(self):
-        self.crawler.insert_child(self.n3)
+        self.crawler.add_child(self.n3)
         self.check_child(self.n1, self.n3)
 
     # test removing
 
     def test_4_remove_head(self):
         c = self.crawler
-        c.insert_after(self.n2, self.n1)
-        c.insert_after(self.n3, self.n2)
+        c.add_after(self.n2, self.n1)
+        c.add_after(self.n3, self.n2)
         c.remove(self.n1)
 
         self.assertRaises(AssertionError, self.check_seq, self.n1, self.n2)
@@ -87,8 +87,8 @@ class tests(test_utils):
 
     def test_5_remove_tail(self):
         c = self.crawler
-        c.insert_after(self.n2, self.n1)
-        c.insert_after(self.n3, self.n2)
+        c.add_after(self.n2, self.n1)
+        c.add_after(self.n3, self.n2)
         c.remove(self.n3)
 
         self.assertRaises(AssertionError, self.check_seq, self.n2, self.n3)
@@ -96,8 +96,8 @@ class tests(test_utils):
 
     def test_6_remove_middle(self):
         c = self.crawler
-        c.insert_after(self.n2, self.n1)
-        c.insert_after(self.n3, self.n2)
+        c.add_after(self.n2, self.n1)
+        c.add_after(self.n3, self.n2)
         c.remove(self.n2)
 
         self.check_seq(self.n1, self.n3)
@@ -106,8 +106,8 @@ class tests(test_utils):
 
     def test_7_remove_child(self):
         c = self.crawler
-        c.insert_child(self.n2, self.n1)
-        c.insert_after(self.n3, self.n2)
+        c.add_child(self.n2, self.n1)
+        c.add_after(self.n3, self.n2)
         c.remove(self.n2)
 
         self.check_child(self.n1, self.n3)
@@ -119,8 +119,8 @@ class tests(test_utils):
 
     def test_8_traverse_sibs(self):
         c = self.crawler
-        c.insert_child(self.n2, self.n1)
-        c.insert_after(self.n3, self.n1)
+        c.add_child(self.n2, self.n1)
+        c.add_after(self.n3, self.n1)
 
         self.assertIs(self.n1, c.current_node)
         self.assertIs(self.n3, c.next_sib())
@@ -128,8 +128,8 @@ class tests(test_utils):
 
     def test_9_traverse_node_flat(self):
         c = self.crawler
-        c.insert_after(self.n2, self.n1)
-        c.insert_after(self.n3, self.n2)
+        c.add_after(self.n2, self.n1)
+        c.add_after(self.n3, self.n2)
 
         self.assertIs(self.n1, c.current_node)
         self.assertIs(self.n2, c.next_node())
@@ -138,8 +138,8 @@ class tests(test_utils):
 
     def test_10_traverse_node_wchild(self):
         c = self.crawler
-        c.insert_child(self.n2, self.n1)
-        c.insert_after(self.n3, self.n1)
+        c.add_child(self.n2, self.n1)
+        c.add_after(self.n3, self.n1)
 
         c = self.crawler
         self.assertIs(self.n1, c.current_node)
@@ -151,8 +151,8 @@ class tests(test_utils):
 
     def test_11_crawl_sibs(self):
         c = self.crawler
-        c.insert_child(self.n2, self.n1)
-        c.insert_after(self.n3, self.n1)
+        c.add_child(self.n2, self.n1)
+        c.add_after(self.n3, self.n1)
 
         gen = c.crawl_sibs()
         self.assertIs(self.n1, gen.next())
@@ -161,8 +161,8 @@ class tests(test_utils):
 
     def test_12_crawl_flat(self):
         c = self.crawler
-        c.insert_after(self.n2, self.n1)
-        c.insert_after(self.n3, self.n2)
+        c.add_after(self.n2, self.n1)
+        c.add_after(self.n3, self.n2)
 
         gen = c.crawl()
         self.assertIs(self.n1, gen.next())
@@ -172,8 +172,8 @@ class tests(test_utils):
 
     def test_13_crawl_wchild(self):
         c = self.crawler
-        c.insert_child(self.n2, self.n1)
-        c.insert_after(self.n3, self.n1)
+        c.add_child(self.n2, self.n1)
+        c.add_after(self.n3, self.n1)
 
         gen = c.crawl()
         self.assertIs(self.n1, gen.next())
@@ -191,9 +191,9 @@ class tests(test_utils):
             n4
         """
         c = self.crawler
-        c.insert_child(self.n2, self.n1)
-        c.insert_child(self.n3, self.n2)
-        c.insert_after(self.n4, self.n1)
+        c.add_child(self.n2, self.n1)
+        c.add_child(self.n3, self.n2)
+        c.add_after(self.n4, self.n1)
 
         gen = c.crawl()
         self.assertIs(self.n1, gen.next())
